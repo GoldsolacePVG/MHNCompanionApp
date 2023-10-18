8d6c348bee50a9aca5d9f1d656974c5337e2548a
GoldsolacePVG <rodrigomicopliego@gmail.com>
2023-10-18 19:26:41 +0100

    Testing Button

--- app/src/main/java/com/infernalstudios/mhncompanionapp/MainActivity.kt
+++ app/src/main/java/com/infernalstudios/mhncompanionapp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.infernalstudios.mhncompanionapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navigationButton_ = findViewById<Button>(R.id.navigationButton_)
+
+        navigationButton_.setOnClickListener {
+            val intent = Intent(this, TestButton::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/infernalstudios/mhncompanionapp/TestButton.kt
@@ -0,0 +1,11 @@
+package com.infernalstudios.mhncompanionapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestButton : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_button)
+    }
+}
